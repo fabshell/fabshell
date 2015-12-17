b071f074c2efaea654a05a91749f9602638fd100
scott.a.idler <scott.a.idler@gmail.com>
2015-12-17 01:34:11 -0800

    fixed up some pathing... -sai

--- pynet.sh
+++ pynet.sh
@@ -4,10 +4,10 @@ set -x
 
 mkdir -p ~/.pynet
 
-curl -sL https://github.com/pynet/pynet/raw/master/pynet.py
+curl -sL https://github.com/pynet/pynet/raw/master/pynet.py > ~/.pynet/pynet.py
 
 curl_args=$(ps -eo args | grep curl | grep pynet | head -n 1)
 
 url=$(echo $curl_args | sed -ne 's/.*\(http[^"]*\).*/\1/p')
 
-exec ~/.pynet/pynet.py --pynet-url $url
+exec python ~/.pynet/pynet.py --pynet-url $url
