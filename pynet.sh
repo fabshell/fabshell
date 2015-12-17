a0abfbfbb353468e369653118a2f33432b1d93fe
scott.a.idler <scott.a.idler@gmail.com>
2015-12-17 01:49:21 -0800

    pass url first then any additional args to pynet/pynet/pynet.py... -sai

--- pynet.sh
+++ pynet.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+shift
+
+args="$@"
+
 curl_args=$(ps -eo args | grep curl | grep pynet | head -n 1)
 
 url=$(echo $curl_args | sed -ne 's/.*\(http[^"]*\).*/\1/p')
@@ -12,4 +16,4 @@ mkdir -p ~/.pynet
 
 curl -sL https://github.com/pynet/pynet/raw/master/pynet.py > ~/.pynet/pynet.py
 
-exec python ~/.pynet/pynet.py --pynet-url $url
+exec python ~/.pynet/pynet.py $url $args
