ac4d71fc55186e009cbcd05dab859c95209f8b60
scott.a.idler <scott.a.idler@gmail.com>
2015-12-17 02:08:47 -0800

    put error guard on url discovered via 'ps -eo args'... -sai

--- pynet.sh
+++ pynet.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+echo "pynet.sh:"
+
 args="$@"
 
 curl_args=$(ps -eo args | grep curl | grep pynet | head -n 1)
 
 url=$(echo $curl_args | sed -ne 's/.*\(http[^"]*\).*/\1/p')
 
-echo "pynet.sh:"
+if [ -z "$url" ]; then
+    echo "url not found using 'ps -eo args'"
+    exit -1
+fi
 
 set -x
 
